7994daa818741d40bbc41627cd5184c00ac50312
obed-tc <castroobed53@gmail.com>
2024-04-20 13:52:53 -0400

    feat: Add button start windows

--- src/components/TaskBar/TaskBar.jsx
+++ src/components/TaskBar/TaskBar.jsx
@@ -1,6 +1,16 @@
 import "./TaskBar.css";
+import logoWindows from "./../../assets/logo-windows.png";
+import InternetExplorerBoton from "../../common/buttons/btn-internet-explorer/InternetExplorerBoton";
 function TaskBar() {
-  return <div className="task-bar"></div>;
+  return (
+    <div className="task-bar">
+      <button className="btn-home">
+        <img src={logoWindows} width="30px" />
+        Inicio
+      </button>
+      <InternetExplorerBoton size="30px" />
+    </div>
+  );
 }
 
 export default TaskBar;
